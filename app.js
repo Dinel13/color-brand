8f36c4a78a36ae7f22cbc96dd9ef699045495065
salahuddin <din@pop-os.localdomain>
2020-09-03 14:02:57 +0800

    second commit

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
-const moongose = require('mongoose')
-const userControler = require('./users-controller')
+const express = require("express");
+const bodyParser = require("body-parser");
+const moongose = require("mongoose");
+const userControler = require("./users-controller");
 
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"); // semua url disinkan
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*"); // semua url disinkan
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
-  app.use('/api/hasil', userControler.simpan)
+app.use("/api/hasil", userControler.simpan);
 
-
-  moongose
+moongose
   .connect(
-    "mongodb+srv://salahuddin:XqW3wRaJLiJiWDD1@cluster0.x7u26.mongodb.net/colorbrand?retryWrites=true&w=majority"
-      )
-  .then(() => app.listen(5000))
+    "mongodb+srv://salahuddin:XqW3wRaJLiJiWDD1@cluster0.x7u26.mongodb.net/colorbrand?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useCreateIndex: true }
+  )
+  .then(() => app.listen(port))
   .catch((err) => console.log(err));
