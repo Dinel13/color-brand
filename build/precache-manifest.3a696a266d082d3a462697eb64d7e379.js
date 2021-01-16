96e5099f47208d33a0d876b56465fbbaa8452aa3
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-01-16 13:52:48 +0800

    just buildh

---
+++ build/precache-manifest.3a696a266d082d3a462697eb64d7e379.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dead7f3e40434e15930663af4b31899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676120e0e0d3a238ec22",
+    "url": "/static/css/main.68ecc34c.chunk.css"
+  },
+  {
+    "revision": "0ba871c79f1e89f27b5b",
+    "url": "/static/js/2.a42d4c64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a42d4c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676120e0e0d3a238ec22",
+    "url": "/static/js/main.a4799b97.chunk.js"
+  },
+  {
+    "revision": "b789813ec8d702c335c8",
+    "url": "/static/js/runtime-main.f81a9ad3.js"
+  },
+  {
+    "revision": "374534bc772905a4c3c29a7f39271784",
+    "url": "/static/media/a.374534bc.jpg"
+  },
+  {
+    "revision": "d873e6f360d8ca35ea3aa04c71287fa0",
+    "url": "/static/media/b.d873e6f3.jpg"
+  },
+  {
+    "revision": "a6929c8ba170ea32250f83808dcb01fc",
+    "url": "/static/media/c.a6929c8b.jpg"
+  },
+  {
+    "revision": "56eeea38a3bfddee2111395a82f72955",
+    "url": "/static/media/d.56eeea38.jpg"
+  }
+]);
